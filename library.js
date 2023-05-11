648949e16c6f2d934f8d021b1615e014c0ce0c95
Shannon Chaulet <shannon.chaulet@gmail.com>
2023-05-11 17:19:51 -0400

    Fix bug with notification bell not updating correctly on hover, as well as search bar

--- library.js
+++ library.js
@@ -639,3 +639,23 @@ drake.on('dragend', function(el) {
   el.style.borderColor = '';
   droppableContainer.style.borderColor = '';
 });
+
+function toggleImage() {
+  var button = document.getElementById("notification");
+  var image = button.querySelector("img");
+
+  if (image) {
+    var currentSrc = image.src;
+    var newSrc = "";
+
+    if (currentSrc.endsWith("Notifications.svg")) {
+      newSrc = currentSrc.replace("Notifications.svg", "Notifications2.svg");
+    } else if (currentSrc.endsWith("Notifications2.svg")) {
+      newSrc = currentSrc.replace("Notifications2.svg", "Notifications.svg");
+    }
+
+    if (newSrc) {
+      image.src = newSrc;
+    }
+  }
+}
